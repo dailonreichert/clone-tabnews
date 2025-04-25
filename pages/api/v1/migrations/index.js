b5f4e0ada85a6de8dbb954f7ba5ba60a2116152c
dailonreichert <41094695+dailonreichert@users.noreply.github.com>
2025-04-25 21:52:16 +0000

    fix migration endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -5,6 +5,8 @@ import database from "infra/database";
 export default async function migrations(request, response) {
   const dbClient = await database.getNewClient();
 
+  //test fix-migration-endpoint branch
+
   const defaultMigrationOptions = {
     dbClient: dbClient,
     dryRun: true,
